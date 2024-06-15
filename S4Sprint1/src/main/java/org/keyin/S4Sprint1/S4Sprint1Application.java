73737aed45c62819cd38b384b61180e2c77d7ce7
Austin <Austin.reid@keyin.com>
2024-06-15 10:48:18 -0230

    Project-init

---
+++ S4Sprint1/src/main/java/org/keyin/S4Sprint1/S4Sprint1Application.java
@@ -0,0 +1,13 @@
+package org.keyin.S4Sprint1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S4Sprint1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S4Sprint1Application.class, args);
+    }
+
+}
---
+++ S4Sprint1/src/test/java/org/keyin/S4Sprint1/S4Sprint1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.keyin.S4Sprint1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S4Sprint1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
