e21d65817b0ac9330f59a2548f8acb3d675bde3c
Danielhayward1223 <daniel.hayward@keyin.com>
2024-06-19 18:02:07 -0230

    Implemented Base Passengers

--- src/main/java/org/keyin/S4Sprint1/Passengers/Passengers.java
+++ src/main/java/org/keyin/S4Sprint1/Passengers/Passengers.java
@@ -1,4 +1,53 @@
 package org.keyin.S4Sprint1.Passengers;
 
 public class Passengers {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+
+    public Passengers(Long id, String firstName, String lastName, String phoneNumber) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Passengers() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    
 }
