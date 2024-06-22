31144d8b3a256febe37d774daaf25c41faf9ceca
Beck <rebecca.pond@keyin.com>
2024-06-22 18:12:02 -0230

    Added relations
    added relations to the other packages, uncomment the passenger stuff after merged.

--- src/main/java/org/keyin/S4Sprint1/Cities/Cities.java
+++ src/main/java/org/keyin/S4Sprint1/Cities/Cities.java
@@ -1,23 +1,31 @@
 package org.keyin.S4Sprint1.Cities;
 
+import org.keyin.S4Sprint1.Airports.Airports;
+//import org.keyin.S4Sprint1.Passengers.Passenger;
+
+import java.util.List;
+
 public class Cities {
     private Long id;
     private String name;
     private String state;
     private int population;
+    private List<Airports> airports;
+//    private List<Passenger> passengers;
 
     // Constructor, getters, and setters
-    public Cities() {
-    }
+    public Cities() {}
 
-    public Cities(Long id, String name, String state, int population) {
+    public Cities(Long id, String name, String state, int population, List<Airports> airports){
+//        List<Passenger> passengers) {
         this.id = id;
         this.name = name;
         this.state = state;
         this.population = population;
+        this.airports = airports;
+//        this.passengers = passengers;
     }
 
-    // Getters and setters
     public Long getId() {
         return id;
     }
@@ -49,4 +57,20 @@ public class Cities {
     public void setPopulation(int population) {
         this.population = population;
     }
+
+    public List<Airports> getAirports() {
+        return airports;
+    }
+
+    public void setAirports(List<Airports> airports) {
+        this.airports = airports;
+    }
+
+//    public List<Passenger> getPassengers() {
+//        return passengers;
+//    }
+//
+//    public void setPassengers(List<Passenger> passengers) {
+//        this.passengers = passengers;
+//    }
 }
