e6857b05e4efde68b67c03af458a77992fc8b473
Austin <Austin.reid@keyin.com>
2024-06-29 05:01:57 -0230

    Changed Airports from Longs to Int

--- src/main/java/org/keyin/S4Sprint1/Airports/Airports.java
+++ src/main/java/org/keyin/S4Sprint1/Airports/Airports.java
@@ -10,40 +10,84 @@ public class Airports {
     private Cities city;
     private List<Aircraft> aircraftID;
 
+    /**
+     * Default constructor for the Airports class.
+     */
     public Airports() {
     }
 
+    /**
+     * Constructs an Airports object with the given airport ID, city, and list of aircraft.
+     *
+     * @param airportID  the unique identifier for the airport
+     * @param city  the city where the airport is located
+     * @param aircraft  the list of aircraft at the airport
+     */
     public Airports(Integer airportID, Cities city,  List<Aircraft>aircraft) {
         this.airportID = airportID;
         this.city = city;
         this.aircraftID = aircraft;
     }
 
-
+    /**
+     * Returns the airport ID.
+     *
+     * @return the airport ID
+     */
     public Integer getAirportID() {
         return airportID;
     }
 
+    /**
+     * Sets the airport ID.
+     *
+     * @param airportID  the airport ID
+     */
     public void setAirportID(Integer airportID) {
         this.airportID = airportID;
     }
 
+    /**
+     * Returns the city where the airport is located.
+     *
+     * @return the city
+     */
     public Cities getCity() {
         return city;
     }
 
+    /**
+     * Sets the city where the airport is located.
+     *
+     * @param city  the city
+     */
     public void setCity(Cities city) {
         this.city = city;
     }
 
+    /**
+     * Returns the list of aircraft at the airport.
+     *
+     * @return the list of aircraft
+     */
     public List<Aircraft> getAircraft() {
         return aircraftID;
     }
 
+    /**
+     * Sets the list of aircraft at the airport.
+     *
+     * @param aircraft  the list of aircraft
+     */
     public void setAircraft(List<Aircraft> aircraft) {
         this.aircraftID = aircraft;
     }
 
+    /**
+     * Returns a string representation of the Airports object.
+     *
+     * @return a string representation of the Airports object
+     */
     @Override
     public String toString() {
         return "Airports{" +
